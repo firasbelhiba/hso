1c8a10da89ecc046e622444185c498597962417d
firasbelhiba <firas.belhiba@esprit.tn>
2025-08-06 14:21:04 +0100

    x

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      external: [],
+      input: 'index.html',
     },
   },
   optimizeDeps: {
